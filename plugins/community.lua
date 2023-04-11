89cb23d7c005463865187176b2fa8a7c86e921cd
Lucas Nunzi <lucas.nunzi@getlife.es>
2023-04-11 23:39:28 +0200

    set community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,13 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.text-case-nvim" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.git.git-blame-nvim" },
 }
