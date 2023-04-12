1533486f4c456ea49eb53270830fe70f060c07ea
Lucas Nunzi <lucas.nunzi@getlife.es>
2023-04-13 00:13:29 +0200

    loas luasnips

--- plugins/core.lua
+++ plugins/core.lua
@@ -74,6 +74,18 @@ return {
   --     }, { mode = "n", prefix = "<leader>" })
   --   end,
   -- },
-  -- require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } },
+  --
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
+      require("luasnip.loaders.from_lua").lazy_load {
+        paths = {
+          "./lua/user/luasnippets",
+        },
+      } -- load snippets paths
+    end,
+  },
+  -- require("luasnip.loaders.from_{vscode,lua}").lazy_load { paths = { "./lua/user/snippets" } } ,
   -- require("luasnip.loaders.from_lua").lazy_load { paths = { "./lua/user/luasnippets" } },
 }
