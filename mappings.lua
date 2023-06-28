d9c25e0172860fb43b769cf588899ccad10da4e3
Lucas Nunzi <lucas.nunzi@life5.com>
2023-06-28 23:24:27 +0200

    update test keystroke to use APP_ENV=test

--- mappings.lua
+++ mappings.lua
@@ -50,7 +50,7 @@ return {
 
       function()
         local file = vim.fn.fnamemodify(vim.fn.expand "%", ":~:.")
-        local command = string.format("!docker exec gf_back sh -c 'php vendor/bin/phpunit %s'", file)
+        local command = string.format("!docker exec gf_back sh -c 'APP_ENV=test php vendor/bin/phpunit %s'", file)
         vim.notify(command)
         vim.cmd(command)
       end,
@@ -60,7 +60,7 @@ return {
 
       function()
         local file = vim.fn.expand "%"
-        local command = string.format("!docker exec gf_back sh -c 'php vendor/bin/phpunit'", file)
+        local command = string.format("!docker exec gf_back sh -c 'APP_ENV=test php vendor/bin/phpunit'", file)
         vim.notify(command)
         vim.cmd(command)
       end,
--- plugins/config/nvim-dap.lua
+++ plugins/config/nvim-dap.lua
@@ -1,33 +1,33 @@
--- return {
---     config = function(plugin, opts)
---       require "plugins.configs.mason-nvim-dap" (plugin, opts) -- include the default astronvim config that calls the setup call
---       -- add more custom luasnip configuration such as filetype extend or custom snippets
---       local dap = require "dap"
---       dap.adapters.php = {
---         type = "executable",
---         command = "node",
---         args = { os.getenv "HOME" .. "/.local/share/vscode-php-debug/out/phpDebug.js" },
---       }
---       dap.configurations.php = {
---         {
---           type = "php",
---           request = "launch",
---           name = "Monorepo backend",
---           pathMappings = {
---             ["/app"] = "${workspaceFolder}",
---           },
---           port = 9003,
---         },
---         {
---           type = "php",
---           request = "launch",
---           name = "Monorepo raiz",
---           pathMappings = {
---             ["/app"] = "${workspaceFolder}/backend",
---           },
---           port = 9003,
---         },
---       }
---       dap.adapters.php = function(callback, config) callback { type = "server", host = "127.0.0.1", port = 9003 } end
---     end,
---   }
+return {
+  config = function(plugin, opts)
+    require "plugins.configs.mason-nvim-dap"(plugin, opts) -- include the default astronvim config that calls the setup call
+    -- add more custom luasnip configuration such as filetype extend or custom snippets
+    local dap = require "dap"
+    dap.adapters.php = {
+      type = "executable",
+      command = "node",
+      args = { os.getenv "HOME" .. "/.local/share/vscode-php-debug/out/phpDebug.js" },
+    }
+    dap.configurations.php = {
+      {
+        type = "php",
+        request = "launch",
+        name = "Monorepo backend",
+        pathMappings = {
+          ["/app"] = "${workspaceFolder}",
+        },
+        port = 9003,
+      },
+      {
+        type = "php",
+        request = "launch",
+        name = "Monorepo raiz",
+        pathMappings = {
+          ["/app"] = "${workspaceFolder}/backend",
+        },
+        port = 9003,
+      },
+    }
+    dap.adapters.php = function(callback, config) callback { type = "server", host = "127.0.0.1", port = 9003 } end
+  end,
+}
