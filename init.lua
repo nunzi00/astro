020f232f3726bab2105206d43935f9c1bd54c862
Lucas Nunzi <lucas.nunzi@life5.com>
2023-10-05 10:12:33 +0200

    add text case

--- init.lua
+++ init.lua
@@ -89,7 +89,7 @@ return {
   plugins = {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.editing-support.refactoring-nvim" },
-    { import = "astrocommunity.editing-support.text-case-nvim" },
+    -- { import = "astrocommunity.editing-support.text-case-nvim" },
     { import = "astrocommunity.pack.php" },
     { import = "astrocommunity.pack.yaml" },
     { import = "astrocommunity.pack.typescript" },
